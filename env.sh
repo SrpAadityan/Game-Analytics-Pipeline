437c00ec6830b9171ea5e58baa79a9e3bf4a1544
SrpAadityan <83580423+SrpAadityan@users.noreply.github.com>
2022-07-31 14:36:03 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -14,19 +14,19 @@
 
 # Project Name
 if [ -z $GOOGLE_CLOUD_PROJECT ]; then
-    GOOGLE_CLOUD_PROJECT="my-project"
+    GOOGLE_CLOUD_PROJECT="game-analytics-pipeline"
 fi
 # Pub/Sub topic
-export MY_TOPIC="avro-records"
+export MY_TOPIC="raw-events"
 # GCS Bucket
-export MY_BUCKET="$GOOGLE_CLOUD_PROJECT""_avro_beam"
+export MY_BUCKET="game-analytics-pipeline-bucket"
 # Avro file GCS output path
-export AVRO_OUT="gs://$MY_BUCKET/out/"
+export AVRO_OUT="gs://game-analytics-pipeline-bucket/avro/"
 # Region for Cloud Pub/Sub and Cloud Dataflow
 export REGION="us-central1"
 # Region for BigQuery
 export BQ_REGION="US"
 # BigQuery dataset name
-export BQ_DATASET="sales"
+export BQ_DATASET="tracking"
 # BigQuery table name
-export BQ_TABLE="orders"
+export BQ_TABLE="raw_events"
